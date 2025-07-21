7cefbc7a4e30b959404f3ab6d28de33cc641d31a
Mythic-botz <priyanshuprasad853@gmail.com>
2025-07-21 12:37:26 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -25,7 +25,7 @@ async def start(client, message: Message):
     await m.delete()
 
     # Send sticker after the text sequence
-    await message.reply_sticker("CAACAgUAAyEFAASNKMWdAAIcOmh90bKpm85UFah6UoxonvI9ZRBGAAJBFwACATHxV2SoOLSt6JSmHgQ")
+    await message.reply_sticker("CAACAgIAAyEFAASNKMWdAAIcQGh95vLDmk8-OKWUxxb33wAB-FuQTwACUzIAAl2WGUjH7ClEYsoarh4E")
 
     # Define buttons for the start message
     buttons = InlineKeyboardMarkup([
