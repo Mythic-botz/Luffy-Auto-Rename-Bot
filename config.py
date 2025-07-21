46264a532588beb3158e49a4c466c7c1fb028c6d
Mythic-botz <priyanshuprasad853@gmail.com>
2025-07-21 17:03:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class Config(object):
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6617544956').split()]
     FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'MythicBots').split(',')
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002475576837"))
-    DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1002876112974"))
+    DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1002629771463"))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
